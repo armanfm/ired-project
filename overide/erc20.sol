49131e5636a61f633371f64e0d24aba7052bb8fa
Armando Freire <armanfm@gmail.com>
2026-04-05 10:21:04 -0300

    Add approval and transfer events to ERC20 contract

--- overide/erc20.sol
+++ overide/erc20.sol
@@ -13,6 +13,14 @@ contract meuERC20{
     string public symbol = "MEU";
     uint8 public decimals = 18;
 
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+
+        constructor() {
+            totalSupply = MAX_SUPPLY;
+            balanceOf[address(this)] = MAX_SUPPLY;
+        }
+
     function transferir(uint _value, address _para) virtual public returns(string memory ){
         require(balanceOf[msg.sender] >= _value, "Saldo insuficiente"); // Verifica se o saldo é suficiente);
         balanceOf[msg.sender] -= _value;
@@ -20,12 +28,20 @@ contract meuERC20{
      
         return "transferido com sucesso";
     }
+
+    function approve(address _spender, uint _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    function transferFrom(address _from, address _to, uint _value) public returns(bool success){
+        require(_value <= balanceOf[_from], "Saldo insuficiente"); // Verifica se o saldo é suficiente
+        require(_value <= allowance[_from][msg.sender], "Saldo insuficiente"); // Verifica se o saldo é suficiente
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
     
-
-
-
-
-
-
-
 }
